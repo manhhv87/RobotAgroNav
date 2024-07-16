337943f4d55e1733decf70882c9afe5236a226ec
manhhv87 <manhhv87@gmail.com>
2024-07-16 17:29:09 +0700

    add code

--- segmentation/configs/mobilenet_v3/agronav_mobilenetv3.py
+++ segmentation/configs/mobilenet_v3/agronav_mobilenetv3.py
@@ -10,11 +10,47 @@ data_preprocessor = dict(size=crop_size)
 
 model = dict(
     data_preprocessor=data_preprocessor,
-    pretrained='open-mmlab://contrib/mobilenet_v3_large')
+    pretrained='open-mmlab://contrib/mobilenet_v3_large',
 
-# Re-config the data sampler.
-train_dataloader = dict(batch_size=4, num_workers=4)
-val_dataloader = dict(batch_size=1, num_workers=4)
+    backbone=dict(
+        type='MobileNetV3',
+        arch='large',
+        out_indices=(1, 3, 16)),
+
+    decode_head=dict(
+        type='LRASPPHead',
+        in_channels=(16, 24, 960),
+        in_index=(0, 1, 2),
+        channels=128,
+        input_transform='multiple_select',
+        dropout_ratio=0.1,
+        num_classes=2,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0))    
+    )
+
+optim_wrapper = dict(
+    _delete_=True,
+    type='AmpOptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.0001, betas=(0.9, 0.999), weight_decay=0.05),
+    paramwise_cfg=None
+)
+
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1500),
+    dict(
+        type='PolyLR',
+        power=1.0,
+        begin=1500,
+        end=160000,
+        eta_min=0.0,
+        by_epoch=False,
+    )
+]
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+train_dataloader = dict(batch_size=2)
+val_dataloader = dict(batch_size=1)
 test_dataloader = val_dataloader
-
-runner = dict(type='IterBasedRunner', max_iters=320000)
